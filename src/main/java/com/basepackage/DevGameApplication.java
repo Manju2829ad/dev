3d0b950716e1a9a56d1a618c31ed1a2480e137eb
Manju2829ad <jasonjackie987@gmail.com>
2025-04-07 19:35:33 +0530

    first commmit

---
+++ src/main/java/com/basepackage/DevGameApplication.java
@@ -0,0 +1,13 @@
+package com.basepackage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basepackage/DevGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basepackage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
